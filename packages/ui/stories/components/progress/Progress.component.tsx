4dfa77666c49d851c6464c53062141dbca77b335
sonhoaing <sonhoai272@gmail.com>
2024-05-14 15:48:10 +0700

    add dynamic voice over

--- packages/ui/stories/components/progress/Progress.component.tsx
+++ packages/ui/stories/components/progress/Progress.component.tsx
@@ -16,7 +16,7 @@ export function Progress(props: IProgressProps) {
   const {stories, classNames} = useStoriesContext();
 
   return (
-    <div className="flex w-full items-center space-x-3 z-20 lg:py-3 pr-4 pl-1">
+    <div className="flex w-full items-center space-x-3 z-20 lg:py-3 pr-1 pl-1">
 
       <CloseIcon onClick={props?.onClick}/>
       <div className="flex-1 relative">
