c3981ee67a459aa9ab9e60374f56d118a860676b
sonhoaing <sonhoai272@gmail.com>
2024-05-24 12:33:44 +0700

    #240524 merge dev to prod

--- packages/ui/stories/components/video/typewriter.tsx
+++ packages/ui/stories/components/video/typewriter.tsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
-const Typewriter = ({ text, speed = 50, progress }: any) => {
+const Typewriter = ({ text, speed = 40, progress }) => {
   const [displayedText, setDisplayedText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    const textLength = Math.floor((progress / 100) * text.length);
-    setDisplayedText(text.substring(0, textLength));
-  }, [progress, text]);
+    if (progress < 100) {
+      const textLength = Math.floor((progress / 100) * text.length);
+      const interval = Math.max(speed, (100 - progress) * speed / 100);
+      const timer = setInterval(() => {
+        setCurrentIndex((prevIndex) => {
+          if (prevIndex < textLength) {
+            setDisplayedText(text.substring(0, prevIndex + 1));
+            return prevIndex + 1;
+          } else {
+            clearInterval(timer);
+            return prevIndex;
+          }
+        });
+      }, interval);
+
+      return () => clearInterval(timer);
+    }
+  }, [progress, text, speed]);
 
   return <div>{displayedText}</div>;
 };
